69d98d9b730bb3674cef3a288d1bf7241c81b272
bozhenko1913 <113594941+bozhenko1913@users.noreply.github.com>
2026-01-16 19:10:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-<script>
-    function toggleMenu() {
-        const overlay = document.getElementById('mobileOverlay');
-        overlay.classList.toggle('active');
-        
-        // Анимация иконок бургера (опционально)
-        document.body.style.overflow = overlay.classList.contains('active') ? 'hidden' : 'auto';
-    }
-</script>
+
